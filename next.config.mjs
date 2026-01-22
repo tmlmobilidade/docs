9842d1dcbdd3f9d51d9b976172b3f45c72314586
LoudNeo <meliodasgamer077@gmail.com>
2026-01-22 17:14:51 +0000

    Refactor GitHub Actions workflow to rename and reorganize paths for the docs directory, ensuring consistent structure and improving clarity in the content copying process.

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@ import { createMDX } from 'fumadocs-mdx/next'
 
 const withMDX = createMDX()
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+// const isGitHubPages = process.env.GITHUB_PAGES === 'true'
 
 /** @type {import('next').NextConfig} */
 const config = {
-  assetPrefix: isGitHubPages ? '/TML/' : '',
-  basePath: isGitHubPages ? '/TML' : '',
+  // assetPrefix: isGitHubPages ? '/TML/' : '',
+  // basePath: isGitHubPages ? '/TML' : '',
   output: 'export',
   reactStrictMode: true,
 }
