6ccaf9f69eb9fb514ba4661ee76bb6a113e75cef
LoudNeo <meliodasgamer077@gmail.com>
2026-01-22 17:49:49 +0000

    Update Next.js configuration to set basePath and assetPrefix for GitHub Pages deployment, ensuring correct static file serving.

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,14 @@ import { createMDX } from 'fumadocs-mdx/next'
 
 const withMDX = createMDX()
 
-// const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',      // gera site estático
   reactStrictMode: true,
-  trailingSlash: true,
+  basePath: isGithubPages ? '/docs' : '',
+  assetPrefix: isGithubPages ? '/docs/' : '',
 }
 
 export default withMDX(config)
