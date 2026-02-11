22d44aa5333beca20d6af80ebbd731a264d23de7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-11 02:03:49 +0000

    Tweak

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,11 @@
 
 import { createMDX } from 'fumadocs-mdx/next'
 
-const withMDX = createMDX()
-
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-
 /** @type {import('next').NextConfig} */
 const config = {
-  assetPrefix: isGitHubPages ? '/docs/' : '',
-  basePath: isGitHubPages ? '/docs' : '',
   output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
 }
 
-export default withMDX(config)
+export default createMDX()(config)
