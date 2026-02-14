5a7d1f2a226fafe87e76895a602cf10a3bbd745b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-14 15:56:14 +0000

    vault backup: 2026-02-14 15:56:14

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -7,6 +7,11 @@ import { css } from '@carrismetropolitana/eslint'
 /* * */
 
 export default {
-  ...css,
-  'unit-allowed-list': ['px', '%', 'fr', 'ms', 'deg', 'vh', 'vw', 'em'],
+	...css,
+	rules: {
+		'unit-allowed-list': ['px', '%', 'fr', 'ms', 'deg', 'vh', 'vw', 'em', 'rem'],
+		"at-rule-no-unknown": [true, { "ignoreAtRules": ["source"] }],
+		"import-notation": "string",
+		"comment-empty-line-before": ["always", { "ignore": ["after-comment"] }]
+	},
 }
