43454451e6704b3694ba3e237dc7a210236ccf7d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-14 15:03:19 +0000

    vault backup: 2026-02-14 15:03:19

--- src/components/brand/LoginButton/index.tsx
+++ src/components/brand/LoginButton/index.tsx
@@ -8,7 +8,12 @@ import styles from './styles.module.css';
 
 export function LoginButton() {
 	return (
-		<a className={styles.container} href={BASE_OPTIONS.go_url} rel="noopener noreferrer" target="_blank">
+		<a
+			className={styles.container}
+			href={BASE_OPTIONS.go_url}
+			rel="noopener noreferrer"
+			target="_blank"
+		>
 			Login
 		</a>
 	);
