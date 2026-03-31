5a1e957ed37cdea920de59cac1189915b350fabd
jusimen <jusi.monteiro@protonmail.com>
2026-03-31 02:38:03 +0100

    Refactor SWR fetcher in BaseProvider to handle HTTP errors using HttpException and HttpResponse for improved error management.

--- src/providers/BaseProvider/index.tsx
+++ src/providers/BaseProvider/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { VehiclePositionContextProvider } from '#/src/app/contexts/VehiclePosition.context';
-import { swrFetcher } from '@tmlmobilidade/utils';
+import { HttpException } from '@tmlmobilidade/consts';
+import { HttpResponse } from '@tmlmobilidade/utils';
 /* * */
 
 import { PropsWithChildren } from 'react';
@@ -13,7 +14,16 @@ export function BaseProvider({ children }: PropsWithChildren) {
 	//
 
 	const swrSettings: SWRConfiguration = {
-		fetcher: swrFetcher,
+		fetcher: async (url: string) => {
+			const res = await fetch(url);
+			const data = await res.json() as HttpResponse<unknown>;
+
+			if (!res.ok) {
+				throw new HttpException(res.status, data.error ?? 'An error occurred');
+			}
+
+			return data.data as unknown;
+		},
 		refreshInterval: 60_000, // 1 minute
 		refreshWhenHidden: true,
 		revalidateIfStale: true,
